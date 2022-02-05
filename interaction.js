2bdd8ec88eaad195d3d8d12775e4654310b73ed3
Recurchen <99056986+Recurchen@users.noreply.github.com>
2022-02-05 17:56:58 -0500

    Add files via upload
    First official push: this is the skeleton of 4 views.

---
+++ interaction.js
@@ -0,0 +1,7 @@
+function login_valid(){
+    var username = document.getElementById("username").value;
+    if (username == "chen"){
+        alert("login successfully");
+        return false;
+    }
+}
